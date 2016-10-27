97167d9db5406a202c8b3b24e5cb2c1ca9dbde1f
Ivan Andrianto <andrianto.ivan@yahoo.co.id>
2016-10-27 21:25:15 +0700

    delete and edit event

--- PayRequest/src/java/EventService/EventService.java
+++ PayRequest/src/java/EventService/EventService.java
@@ -74,9 +74,26 @@ public class EventService {
         return events;
     }
 
-    public Event delete(java.lang.String id) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    @WebMethod(operationName = "deleteEvent")
+    @WebResult(name = "Event")
+    public String deleteEvent(@WebParam(name = "ID") int ID) {
+        String status = "";
+        
+        try {
+            new Driver();
+            Connection connection = DriverManager.getConnection(dbAddress,dbUsername,dbPassword);
+            Statement statement = connection.createStatement();
+            
+            String deleteEventQuery = "DELETE  FROM events WHERE id = ? ";
+            PreparedStatement getEventsStatement = connection.prepareStatement(deleteEventQuery);
+            getEventsStatement.setInt(1, ID);
+            getEventsStatement.executeUpdate();
+            status = "success";
+        } catch (SQLException ex) {
+            status = "failed";
+            Logger.getLogger(EventService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return status;
     }
     
     @WebMethod(operationName = "createEvent")
@@ -176,9 +193,102 @@ public class EventService {
         return event;
     }
 
-    public Event edit(java.lang.String address, java.lang.String city, java.lang.String country, javax.xml.datatype.XMLGregorianCalendar createdDate, java.lang.String description, javax.xml.datatype.XMLGregorianCalendar eventTimeEnd, javax.xml.datatype.XMLGregorianCalendar eventTimeStart, java.lang.String id, java.lang.String name, java.lang.String pictureUrl, java.lang.String postalCode, java.lang.String state, java.lang.String status, java.util.List<java.lang.String> tags, java.lang.String type, javax.xml.datatype.XMLGregorianCalendar udpatedDate, java.lang.String userId) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    
+    @WebMethod(operationName = "editEvent")
+    @WebResult(name = "Event")
+    public Event editEvent(
+        @WebParam(name = "ID") int ID,
+        @WebParam(name = "name") String name,
+        @WebParam(name = "address") String address,
+        @WebParam(name = "city") String city,
+        @WebParam(name = "state") String state,
+        @WebParam(name = "country") String country,
+        @WebParam(name = "postalCode") int postalCode,
+        @WebParam(name = "description") String description,
+        @WebParam(name = "pictureUrl") String pictureUrl,
+        @WebParam(name = "status") String status,
+        @WebParam(name = "tags") String tags,
+        @WebParam(name = "type") String type,
+        @WebParam(name = "userID") int userID,
+        @WebParam(name = "startTime") Date startTime,
+        @WebParam(name = "endTime") Date endTime
+    ) {
+        Event event = new Event();
+
+        try {
+            new Driver();
+            Connection connection = DriverManager.getConnection(dbAddress,dbUsername,dbPassword);
+            Statement statement = connection.createStatement();
+            
+            String createEventQuery = "UPDATE events SET name = ?, address = ?, city = ?, state = ?,"
+                    + " country = ?, postalCode = ?, description = ?, pictureUrl = ?, status = ?, tags = ?, "
+                    + " type =  = ?, userID = ?, startTime  = ?, endTime = ? WHERE ID = ?";
+
+            PreparedStatement createEventStatement = connection.prepareStatement(createEventQuery);
+            createEventStatement.setString(1, name);
+            createEventStatement.setString(2, address);
+            createEventStatement.setString(3, city);
+            createEventStatement.setString(4, state);
+            createEventStatement.setString(5, country);
+            createEventStatement.setInt(6, postalCode);
+            createEventStatement.setString(7, description);
+            createEventStatement.setString(8, pictureUrl);
+            createEventStatement.setString(9, status);
+            createEventStatement.setString(10, tags);
+            createEventStatement.setString(11, type);
+            createEventStatement.setInt(12, userID);
+            createEventStatement.setDate(13, new java.sql.Date(startTime.getTime()));
+            createEventStatement.setDate(14, new java.sql.Date(endTime.getTime()));
+            createEventStatement.setInt(15, ID);
+            createEventStatement.executeUpdate();
+            ResultSet rs = createEventStatement.getGeneratedKeys();
+            
+            int eventID = 0;
+            while (rs.next()) {
+                eventID = rs.getInt(1);
+            }
+            
+            if (eventID != 0) {
+                String getEventQuery = "SELECT * FROM events WHERE id = ?";
+                PreparedStatement getEventStatement = connection.prepareStatement(getEventQuery);
+                getEventStatement.setInt(1, ID);
+                ResultSet rs2 = getEventStatement.executeQuery();
+                
+                if (rs2.next()) {
+                    String _name = rs2.getString("name");
+                    String _address = rs2.getString("address");
+                    String _city = rs2.getString("city");
+                    String _state = rs2.getString("state");
+                    String _country = rs2.getString("country");
+                    int _postalCode = rs2.getInt("postalCode");
+                    String _description = rs2.getString("description");
+                    String _pictureUrl = rs2.getString("pictureUrl");
+                    String _status = rs2.getString("status");
+                    String _tags = rs2.getString("tags");
+                    String _type = rs2.getString("type");
+                    int _userID = rs2.getInt("userID");
+                    Date _created = new Date(rs2.getTimestamp("created").getTime());
+                    Date _updated = new Date(rs2.getTimestamp("updated").getTime());
+                    Date _startTime = new Date(rs2.getTimestamp("startTime").getTime());
+                    Date _endTime = new Date(rs2.getTimestamp("endTime").getTime());
+
+                    event = new Event (ID, _address, _city, _country, _created,
+                            _description, _endTime, _name, _pictureUrl,
+                            _postalCode, _startTime, _state, _status,
+                            _tags, _type, _userID, _updated);
+
+                    rs2.close();
+                    statement.close();
+                    connection.close();   
+                }
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(EventService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return event;
     }
 
     @WebMethod(operationName = "getEventDetail")
